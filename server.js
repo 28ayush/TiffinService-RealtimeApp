0dda09f7391ec9d82654a3d123d9d50326c6c04c
Ayush Varshney <28ayushvarshney@gmail.com>
2023-03-03 00:28:17 +0530

    make home page

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT=process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname+'/resources/views'));
+//assests
+app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
     res.render('home');
